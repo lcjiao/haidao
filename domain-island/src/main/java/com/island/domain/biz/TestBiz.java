5cd33a8c9fe56bb382342168b37064f2f7c30fe5
lcjiao <lcjiao@anjuke.com>
2014-04-11 21:31:52 +0800

    change pox

--- domain-island/src/main/java/com/island/domain/biz/TestBiz.java
+++ domain-island/src/main/java/com/island/domain/biz/TestBiz.java
@@ -1,11 +1,14 @@
 package com.island.domain.biz;
 
+import org.apache.commons.configuration.Configuration;
+
+import com.jcl.core.config.AbsCfgListener;
 import com.island.domain.DomainIslandModule;
 import com.island.domain.dal.BrokerPpcCeilingLogIbatisDAOImpl;
 import com.island.domain.model.BrokerPpcCeilingLog;
 import com.jcl.core.module.ModuleRegistry;
 
-public class TestBiz {
+public class TestBiz extends AbsCfgListener{
 	
 	private BrokerPpcCeilingLogIbatisDAOImpl testDao;
 
@@ -22,10 +25,31 @@ public class TestBiz {
 		this.testDao.insert(model);
 		System.out.println(model.getId());
 	}
+	
+	public void testConf(){
+		for(int i=0;i<100;i++){
+			System.out.println("index"+i+",confValue:"+confValue);
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 
 	public static void main(String arg[]){
 		TestBiz biz = ModuleRegistry.getInstance()
 				.getModule(DomainIslandModule.class).getTestBiz();
-		biz.addModel();
+		biz.testConf();
+	}
+
+	private String confValue;
+	@Override
+	public void load() {
+		Configuration config = this.getDependencyConfig().get(0);
+
+		confValue = config
+				.getString("test_conf");
 	}
 }
