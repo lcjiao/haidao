a1d510fba421677e7483cae09fc8b4cec1525c8d
lcjiao <lcjiao@anjuke.com>
2014-04-22 21:18:38 +0800

    del file

--- domain-island/src/main/java/com/island/domain/dal/mapper/BrokerPpcCeilingLogMapper.java
+++
@@ -1,9 +0,0 @@
-package com.island.domain.dal.mapper;
-
-
-import com.island.domain.model.BrokerPpcCeilingLog;
-import com.jcl.core.dal.ibatis.mapper.IMapper;
-
-public interface BrokerPpcCeilingLogMapper extends IMapper<BrokerPpcCeilingLog>{
-
-}
