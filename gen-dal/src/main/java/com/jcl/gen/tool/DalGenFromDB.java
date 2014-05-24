09ab2aa61a845801e7dc5e50e0df18ba60a381ae
zhoubl <zhoubl@126.com>
2014-05-24 10:30:12 +0800

    不知道为什么

--- gen-dal/src/main/java/com/jcl/gen/tool/DalGenFromDB.java
+++ gen-dal/src/main/java/com/jcl/gen/tool/DalGenFromDB.java
@@ -19,7 +19,7 @@ public class DalGenFromDB extends DalGen {
 	}
 
 	public static void main(String[] args) throws Exception {
-		DalGenFromDB hf = new DalGenFromDB("haidao_db","test_tab",
+		DalGenFromDB hf = new DalGenFromDB("haidao_db","package_detail_info",
 				new DataSourceDefinition[] { DataSourceDefinition.HAIDAO_DB,DataSourceDefinition.HAIDAO_DB});
 		hf.process();
 	}
