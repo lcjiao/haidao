0f3afccea9b6ba53d8cd8e71fbf7e4388134986f
zhoubl <zhoubl@126.com>
2014-06-12 22:01:53 +0800

    修改

---
+++ domain-island/src/main/java/com/island/domain/biz/RecommendBiz.java
@@ -0,0 +1,44 @@
+package com.island.domain.biz;
+
+import java.util.List;
+import java.util.Map;
+
+import com.island.domain.dal.RecommendIbatisDAOImpl;
+import com.island.domain.model.Recommend;
+
+public class RecommendBiz {
+
+	private RecommendIbatisDAOImpl recommendDao;
+
+	public void setRecommendDao(RecommendIbatisDAOImpl recommendDao) {
+		this.recommendDao = recommendDao;
+	}
+	
+	public List<Recommend> queryByMap(Map<String,Object> params){
+		return this.recommendDao.queryByMap(params);
+	}
+	
+	public Integer countByMap(Map<String,Object> params){
+		return this.recommendDao.countByMap(params);
+	}
+	
+	public void addMasterRecommend(Recommend obj){
+		this.recommendDao.insert(obj);
+	}
+	
+	public void updRecommend(Map<String,Object> params){
+		this.recommendDao.updateByMap(params);
+	}
+	
+	public void updRecommendByModel(Recommend obj){
+		this.recommendDao.update(obj);
+	}
+	
+	public Recommend queryById(Integer id){
+		return this.recommendDao.queryById(id);
+	}
+	
+	public void updateByAreaIsland(Map<String,Object> params){
+		this.recommendDao.updateByAreaIsland(params);
+	}
+}
---
+++ domain-island/src/main/java/com/island/domain/dal/RecommendIbatisDAOImpl.java
@@ -0,0 +1,57 @@
+package com.island.domain.dal;
+
+
+import java.util.List;
+import java.util.Map;
+
+
+import java.util.Map;
+
+import org.apache.ibatis.session.SessionException;
+import org.apache.ibatis.session.SqlSession;
+
+import com.jcl.core.dal.datasource.DataSourceDefinition;
+import com.jcl.core.dal.ibatis.AbsiBatisDAOImpl;
+import com.jcl.core.dal.ibatis.SqlmapUtils;
+import com.island.domain.dal.mapper.RecommendMapper;
+import com.island.domain.dal.mapper.UserMapper;
+import com.island.domain.model.Recommend;
+
+public class RecommendIbatisDAOImpl extends AbsiBatisDAOImpl<Recommend> {
+	static {
+		SqlmapUtils.addMapper(RecommendMapper.class,DataSourceDefinition.HAIDAO_DB);
+    }
+    @Override
+	public Class<Recommend> getModelCls() {
+		
+		return Recommend.class;
+	}
+	@Override
+	public Class<RecommendMapper> getMapperCls() {
+		
+		return RecommendMapper.class;
+	}
+	
+	@Override
+	public DataSourceDefinition getDefWriteDB() {		
+		return DataSourceDefinition.HAIDAO_DB;
+	}
+	
+	@Override
+	public DataSourceDefinition getDefQueryDB() {
+		return DataSourceDefinition.HAIDAO_DB;
+	}
+	
+	public void updateByAreaIsland(Map<String,Object> params){
+		SqlSession session = SqlmapUtils.openSession(DataSourceDefinition.HAIDAO_DB);
+		try {
+			RecommendMapper mapper = session.getMapper(RecommendMapper.class);
+			mapper.updateByAreaIsland(params);
+		} catch (SessionException e) {
+			e.printStackTrace();
+		} finally {
+			session.commit();
+			session.close();
+		}
+	}
+}
